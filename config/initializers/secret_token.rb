4f62290700aa28fcc2152d0287672c899969ac92
Jo Liss <joliss42@gmail.com>
2012-06-19 21:07:50 +0200

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterTweetFeed::Application.config.secret_token = '2f269fd6ac5b2553a3477b7981928bd76b7820190650ac24f4db8413d292b41d03d1ff17c5b7d33394368358c29a087cb85f989ff764b2f6f4aa70e6ea0a0d16'
